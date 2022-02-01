5aed4d6d475e0e36d79812d9ada63910147ba507
Serena Villa <josie.jp@gmail.com>
2022-02-01 16:10:22 +0100

    Plesk timezone fix

---
+++ plesk-horde-timezone-fix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## Description: Horde Timezone Fix for Plesk Webserver ##
+## Author: Serena Villa <josie.jp@gmail.com>
+
+SEARCH="date.timezone"
+APPEND="date.timezone = \"Europe/Rome\""
+FILE="/etc/psa-webmail/horde/horde/php.ini"
+
+if  grep -q -F "$SEARCH" "$FILE"; then
+  sed -i '/date.timezone/d' $FILE
+  echo $APPEND >> $FILE
+fi
